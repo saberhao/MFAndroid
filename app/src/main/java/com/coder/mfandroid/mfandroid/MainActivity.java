9d9ccdcd6cf976a975ffe69c546f15c4e0aaa4d0
saberhao <565776551@qq.com>
2016-02-24 15:32:41 +0800

    Initial commit

---
+++ app/src/main/java/com/coder/mfandroid/mfandroid/MainActivity.java
@@ -0,0 +1,10 @@
+package com.coder.mfandroid.mfandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by saberhao on 2016/2/24.
+ */
+public class MainActivity extends AppCompatActivity {
+
+}
